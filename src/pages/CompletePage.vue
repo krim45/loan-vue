ecde33253687a9e81ced4d6191152360ca7742df
krim45 <krim45@naver.com>
2022-04-13 14:38:21 +0900

    -feat: Complete Page

--- src/pages/CompletePage.vue
+++ src/pages/CompletePage.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    
-  </div>
+  <TheHeader />
+  <h3 class="top3">대출이 완료됐어요!</h3>
+  <img
+    class="complete-img"
+    src="https://static.toss.im/3d-emojis/u1F389_apng.png"
+  />
+  <NextButton path="/start">확인</NextButton>
 </template>
 
 <script>
+import TheHeader from "@/components/TheHeader.vue";
+import NextButton from "@/components/NextButton.vue";
 export default {
-  
-}
+  components: { TheHeader, NextButton },
+};
 </script>
 
 <style>
-  
+.complete-img {
+  display: block;
+  margin: 60px auto;
+  width: 200px;
+  height: 200px;
+}
 </style>
