da3cb206d767eb59b229917753b329e6f0b543c7
krim45 <krim45@naver.com>
2022-04-13 14:39:44 +0900

    -feat: Confirmation Page

--- src/pages/ConfirmationPage.vue
+++ src/pages/ConfirmationPage.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-    
-  </div>
+  <TheHeader />
+  <h3 class="top3">대출 내용을 확인할게요</h3>
+  <ul class="list-contianr">
+    <li class="list">
+      <span>대출금액</span>
+      <span>{{ formatPrice }}</span>
+    </li>
+    <li class="list">
+      <span>상환기한</span>
+      <span>{{ duration }}개월</span>
+    </li>
+  </ul>
+  <NextButton path="/complete">확인</NextButton>
 </template>
 
 <script>
+import TheHeader from "@/components/TheHeader.vue";
+import NextButton from "@/components/NextButton.vue";
+import { clearItem } from "@/utils/localStorage";
 export default {
-  
-}
+  components: { TheHeader, NextButton },
+  props: ["duration", "formatPrice"],
+  // emits: ["update-loan", "update-duration"],
+  mounted() {
+    clearItem();
+  },
+};
 </script>
 
-<style>
-  
+<style scoped>
+.list-contianr {
+  display: flex;
+  flex-direction: column;
+  padding: 0 24px;
+  margin: 16px 0;
+}
+
+.list {
+  display: flex;
+  justify-content: space-between;
+}
+
+span {
+  color: rgb(25, 31, 40);
+  font-size: 20px;
+  line-height: 1.5;
+}
 </style>
